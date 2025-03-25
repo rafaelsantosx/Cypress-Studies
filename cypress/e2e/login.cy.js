c614b0df8bd68c9e6b24f7bee83c812e7016977f
rafaelsantosx <rafaelsantosdeoliveirax@gmail.com>
2025-03-25 01:48:50 -0300

    Módulo 6 - Parte 1

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,13 +4,20 @@ describe('Login', () =>{
         //get () - encontrar ou selecionar elementos
         cy.visit('/').get('.header-logo')
 
-        //contains() - para encontrar elementos por texto onde geralmente diminuimos o escopo com um get()
+        //contains() - para encontrar elementos *por texto* onde geralmente diminuimos o escopo com um get()
+        //contains é para texto
         cy.get('#top_header').contains('Login')
 
-        //find()
+        //find() é para encontrar elementos
         cy.get('#top_header').find('.fa-user')
+        // ponto (.) é para classes (#) é para id
+
+        //alis
+        //as () --- criar apelidos para (atalhos) para grandes comandos
+        cy.get('#top_header').as('cabecalho')//criando atalho
+
+        cy.get('@cabecalho').contains('Cadastro')//utlizando atalho
 
 
-        //as () - alias
     })
 })
